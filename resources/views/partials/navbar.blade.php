7ad837c4616790bab1f1e587616cefa4b36715ef
Galih Gustika <galihdrm@gmail.com>
2022-09-09 09:11:08 +0700

    view Login dan Register

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -10,15 +10,20 @@
             <a class="nav-link {{ ($active =="home") ? 'active' : '' }}" href="/">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link {{ ($active =="about") ? 'active' : '' }}"" href="/about">About</a>
+            <a class="nav-link {{ ($active =="about") ? 'active' : '' }}" href="/about">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link {{ ($active =="posts") ? 'active' : '' }}"" href="/posts">Blog</a>
+            <a class="nav-link {{ ($active =="posts") ? 'active' : '' }}" href="/posts">Blog</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link {{ ($active =="caregories") ? 'active' : '' }}"" href="/categories">Categories</a>
+            <a class="nav-link {{ ($active =="caregories") ? 'active' : '' }}" href="/categories">Categories</a>
           </li>
         </ul>
+        <div class="navbar-nav ms-auto">
+          <div class="nav-item ">
+            <a class="nav-link {{ $active =="login" ? 'active' : '' }} " href="/login"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+          </div>
+        </div>
       </div>
     </div>
 </nav>  
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\User;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 
 /*
@@ -73,7 +75,10 @@ Route::get('/categories',function(){
     ]);
 });
 
-Route::get('/categories/{category:slug}',function(Category $category){
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class,  'index']);
+
+/* Route::get('/categories/{category:slug}',function(Category $category){
     return view('posts ',[
         "title" => "Posts By Category : $category->name ",
         "active" => "categories",
@@ -87,4 +92,4 @@ Route::get('/authors/{author:username}', function(User $author){
         "active" => "posts",
         "posts" => $author->posts->load('category','author'),
     ]);
-});
+}); */
